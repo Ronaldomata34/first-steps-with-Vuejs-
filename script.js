6d872a13f928e461cf0bd8f604b5da78cb32d367
Ronaldo Mata <ronaldomata34@gmail.com>
2018-03-05 11:52:48 -0400

    my first steps with Vuejs

---
+++ script.js
@@ -0,0 +1,14 @@
+new Vue({
+	el: '#appVue',
+	data: {
+		lists: [],
+		newKeep: ""
+	},
+	methods: {
+		addKeep: function() {
+			this.lists.push({keep: this.newKeep, completed: false});
+			this.newKeep = "";
+		}
+	}
+})
+
